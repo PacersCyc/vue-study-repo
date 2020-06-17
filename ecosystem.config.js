1e3a1f4c7dd9fc31eb83667fd82214ef78170f22
PacersCyc <452720855@qq.com>
2020-06-17 22:39:59 +0800

    添加pm2配置

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps : [{
+    name: 'VUE-SSR-TODO',
+    script: './server/server.js',
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env_production: {
+      NODE_ENV: 'production',
+      HOST: 'localhost',
+      PORT: 8888
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
